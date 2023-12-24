4a7279821b78649c9e75e13382e38b47ea2c1b94
Lucas Larroche <lucas@larroche.com>
2023-12-24 17:13:21 +0700

    fix: meta

--- app/root.jsx
+++ app/root.jsx
@@ -114,6 +114,9 @@ export default function App() {
       <ModalProvider>
         <PageProvider>
           <head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <meta name="theme-color" content="#2a3140" />
             <Meta />
             <Links />
             <DynamicMeta />
