269b86ef9f6321c15ba1a8ca02ac0baf4572a946
Lucas Larroche <lucas@larroche.com>
2023-12-24 16:42:56 +0700

    build: update to Remix 2

---
+++ remix.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  server: "./server.js",
+  serverBuildPath: "functions/[[path]].js",
+  serverConditions: ["worker"],
+  serverDependenciesToBundle: "all",
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
+};
