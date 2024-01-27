7120a815987655e2694b68024fa61960f9f82423
Lucas Larroche <lucas@larroche.com>
2024-01-27 13:20:56 +0700

    feat: better color schemes doc

--- app/components/docs/Main.jsx
+++ app/components/docs/Main.jsx
@@ -19,7 +19,6 @@ export default function Main({ children, ...props }) {
   const pageId = getPageId(routePath);
 
   const pageIdsWithoutTableOfContents = [
-    "color-schemes",
     "rtl",
     "theme-generator",
     "scroller",
--- app/routes/docs.color-schemes.jsx
+++ app/routes/docs.color-schemes.jsx
@@ -1,8 +1,10 @@
+import { useRef } from "react";
 import Code from "~/components/Code";
 import Heading from "~/components/Heading";
 import Content from "~/components/docs/Content";
 import EditOnGithub from "~/components/docs/EditOnGithub";
 import Header from "~/components/docs/Header";
+import TableOfContents from "~/components/docs/TableOfContents";
 import ThemeToggle from "~/components/icons/ThemeToggle";
 import { usePage } from "~/contexts/PageContext";
 import metaData from "~/data/meta";
@@ -14,7 +16,7 @@ export const meta = () => [
   {
     name: "description",
     content:
-      "Choose from two consistent color schemes that can be automatically enabled based on users' preferences.",
+      "Pico CSS comes with both Light and Dark color schemes, automatically enabled based on user preferences.",
   },
 ];
 
@@ -22,18 +24,42 @@ export default function ColorSchemes() {
   const { pageTheme, switchTheme } = usePage();
   const isThemeDark = pageTheme === "dark";
   const changeThemeLabel = isThemeDark ? "Turn off dark mode" : "Turn on dark mode";
+  const introductionRef = useRef();
+  const usageRef = useRef();
+  const exampleRef = useRef();
 
   return (
     <>
       {/* Header */}
       <Header
         title="Color schemes"
-        description="Choose from two consistent color&nbsp;schemes that can be automatically enabled based on users'&nbsp;preferences."
+        description="Pico CSS comes with both Light and Dark color schemes, automatically enabled based on user preferences."
+      />
+
+      {/* Table of content */}
+      <TableOfContents
+        data={[
+          {
+            anchor: "",
+            title: "Introduction",
+            ref: introductionRef,
+          },
+          {
+            anchor: "usage",
+            title: "Usage",
+            ref: usageRef,
+          },
+          {
+            anchor: "example",
+            title: "Card example",
+            ref: exampleRef,
+          },
+        ]}
       />
 
       {/* Content */}
       <Content>
-        <section>
+        <section ref={introductionRef}>
           <p>
             The default color scheme is Light. The Dark scheme is automatically enabled if the user
             has dark mode enabled{" "}
@@ -45,12 +71,43 @@ export default function ColorSchemes() {
               {changeThemeLabel}
             </button>
           </article>
-
+        </section>
+        <section ref={usageRef}>
+          <Heading level={2} anchor="usage">
+            Usage
+          </Heading>
           <p>
-            Color schemes can be forced on document level{" "}
-            <Code display="inline">{`<html data-theme="light">`}</Code> or on any HTML element{" "}
-            <Code display="inline">{`<article data-theme="dark">`}</Code>
+            Color schemes can be defined for the entire document using{" "}
+            <Code display="inline">{`<html data-theme="light">`}</Code> or for specific HTML
+            elements, such as <Code display="inline">{`<article data-theme="dark">`}</Code>.
           </p>
+          <p>
+            Color schemes at the HTML tag level work great for elements such as{" "}
+            <Code display="inline">a</Code>, <Code display="inline">button</Code>,{" "}
+            <Code display="inline">table</Code>, <Code display="inline">input</Code>,{" "}
+            <Code display="inline">textarea</Code>, <Code display="inline">select</Code>,{" "}
+            <Code display="inline">article</Code>, <Code display="inline">dialog</Code>,{" "}
+            <Code display="inline">progress</Code>.
+          </p>
+          <p>
+            CSS variables specific to the color scheme are assigned to every HTML tag. However, to
+            maintain transparent backgrounds and ensure colors are inherited from the parent tag, we
+            have not enforced specific background and color settings across all HTML tags.
+          </p>
+          <p>
+            For some other HTML tags, you might need to explicitly set{" "}
+            <Code display="inline">background-color</Code> and <Code display="inline">color</Code>.
+          </p>
+          <Code language="css">{`section {
+  background-color: var(--pico-background-color);
+  color: var(--pico-color);
+}`}</Code>
+        </section>
+        <section ref={exampleRef}>
+          <Heading level={2} anchor="example">
+            Card example
+          </Heading>
+
           <article data-theme="light" aria-label="Forced light theme example">
             <Heading level={2}>Light card</Heading>
             <form>
