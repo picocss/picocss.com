b0c173d84aa140068a776419a93628fbe5e7a090
Lucas Larroche <lucas@larroche.com>
2024-01-28 09:04:09 +0700

    copy: quick start

--- app/routes/docs._index.jsx
+++ app/routes/docs._index.jsx
@@ -130,6 +130,7 @@ export default function Docs() {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark" />
     <link rel="stylesheet" href="css/pico.min.css">
     <title>Hello, world!</title>
   </head>
